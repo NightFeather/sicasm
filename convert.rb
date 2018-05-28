945b6bfbc79b01ae6eb0707897c9f8ff67ed73dd
Nightfeather <ss900328@gmail.com>
2018-05-29 02:13:37 +0800

    Add error messages
    and script

---
+++ convert.rb
@@ -0,0 +1,30 @@
+require 'json'
+puts \
+  ARGF.each_line
+    .map(&:split)
+    .map { |op,arg,form,code|
+      if arg =~ /m|r|n/
+        arg = arg.split(",").map do |a|
+          case a
+          when /r\d/
+            :register
+          when 'm'
+            :general
+          when 'n'
+            :numeric
+          end
+        end
+      else
+        arg, form, code = [], arg, form
+      end
+      [op,arg,form,code]
+    }.map { |op,arg, form, code|
+      [
+        op,
+        {
+          args: arg,
+          formats: form.split(?/).map(&:to_i),
+          code: code.to_i(16)
+        }
+      ]
+    }.to_h.to_json
